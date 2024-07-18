ff24e952264f29dd3d22b6b943c7c65d338d74c3
-g <-g>
2024-07-18 17:43:45 +0330

    update on footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,16 +4,19 @@ import { DynamicSvg } from "../../assets/icons/icons";
 const Footer = () => {
   return (
     <footer className=" relative rounded-tl-2xl  flex   border-r-[4rem] border-b-primary justify-center border-b-[4rem] border-transparent">
-      <div className=" absolute w-full flex items-center justify-between max-w-screen-2xl   p-8 h-[50px] md:h-[50px]  ">
-        <div className="text-white font-extralight text-[8px]">
-          Developed By: a-team
+      <div className=" absolute w-full ml-16  flex items-center justify-between max-w-screen-xl   p-8 h-[50px] md:h-[50px]  ">
+      
+          <div className="text-white text-left font-extralight text-[10px]">
+            <div> Designed By: <a href="#"> Raana Sheykhi </a> </div>
+          Developed By: <a href="#"> Amir Samimi </a>,<a href="#"> Alireza Gosili </a>,<a href="#"> Niloofar </a> 
         </div>
-        <div className="flex justify-between gap-4 w-20 md:36">
-          <DynamicSvg name="instagram-bold" color="white" />
-          <DynamicSvg name="telegram-bold" color="white" />
-          <DynamicSvg name="whatsapp-bold" color="white" />
+        <div className="flex justify-between gap-4 w-24 ">
+          <DynamicSvg size="24" name="instagram-bold" color="white" />
+          <DynamicSvg size="24" name="telegram-bold" color="white" />
+          <DynamicSvg size="24" name="whatsapp-bold" color="white" />
         </div>
       </div>
+ 
     </footer>
   );
 };
